6e96b3e92d473eef5654a57f547e967b8c0e6745
Bernhard Utz <if21b241@technikum-wien.at>
2023-11-14 15:53:35 +0100

    changed return to optional

--- readFile.cpp
+++ readFile.cpp
@@ -1,15 +1,20 @@
 #include <iostream>
 #include <fstream>
 #include <sstream>
+#include <optional>
 
 
-auto readFile = [](const std::string path){
-  // filepfad als String
+auto readFile = [](const std::string path) -> std::optional<std::string>
+{
   std::ifstream file;
   file.open(path);
 
+  if (!file) {
+    return std::nullopt;
+  }
+
   std::stringstream buffer;
   buffer << file.rdbuf();
+  file.close();
   return buffer.str();
-
 };
