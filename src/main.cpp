4d3c663173ac92c75116c4c836873b51701c2ce1
Bernhard Utz <if21b241@technikum-wien.at>
2023-11-20 13:16:26 +0100

    put everything into the main

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include "tokenizer.h"
+#include "readFile.h"
+#include "readAndTokenize.h"
 
 int main(int argc, char* argv[]) {
-    std::cout << "Hello, World!" << std::endl;
+    auto args = readAndTokenize::validateArgs(argc, argv);
+    if (!args.has_value()) {
+        // ToDo: Error handling
+        return 1;
+    }
+
+    auto peaceTermsFile = readFile::readFile(args.value()[1]);
+    auto warTermsFile = readFile::readFile(args.value()[2]);
+    auto textFile = readFile::readFile(args.value()[3]);
+
+    if(!peaceTermsFile.has_value() || !warTermsFile.has_value() || !textFile.has_value()) {
+        return 1;
+    }
+
+    auto peaceTerms = tokenizer::tokenize(peaceTermsFile.value()); 
+    auto warTerms = tokenizer::tokenize(warTermsFile.value()); 
+    auto text = tokenizer::tokenize(textFile.value()); 
+
     return 0;
 }
