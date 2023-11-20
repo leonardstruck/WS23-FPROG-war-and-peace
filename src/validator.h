e59fef274ab638b1663bc0b03c86bc3dd5ae8694
Bernhard Utz <if21b241@technikum-wien.at>
2023-11-20 13:19:24 +0100

    cahnged filename

---
+++ src/validator.h
@@ -0,0 +1,22 @@
+#ifndef VALIDATOR_H
+#define VALIDATOR_H
+
+#include <iostream>
+#include <vector>
+#include <string>
+#include <range/v3/all.hpp>
+#include "tokenizer.h"
+#include "readFile.h"
+
+namespace validator {
+  auto validateArgs = [](int argc, char* argv[]) -> std::optional<std::vector<std::string>> {
+    // es müssen genau drei Dateien übergeben werden (book, war terms, peace terms)
+    if (argc != 4) {
+        return std::nullopt;
+    }
+    std::vector<std::string> args(argv, argv + argc);
+    return args;
+  };
+}
+
+#endif // VALIDATOR_H
