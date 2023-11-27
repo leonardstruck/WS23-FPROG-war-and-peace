53a6ecb3af54f472cc22a5fb71af3b0d2cbda97d
Leonard Struck <me@leonardstruck.com>
2023-11-27 17:30:46 +0100

    add error message and run configuration for clion

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,9 @@
 int main(int argc, char* argv[]) {
     auto args = validator::validateArgs(argc, argv);
     if (!args.has_value()) {
-        // ToDo: Error handling
+        std::cout << "Invalid arguments" << std::endl;
+        std::cout << "Please provide the paths to the book, war terms and peace terms files" << std::endl;
+        std::cout << "Usage: " << "war_and_peace" << " <peace terms> <war terms> <text>" << std::endl;
         return 1;
     }
 
@@ -15,6 +17,7 @@ int main(int argc, char* argv[]) {
     auto textFile = readFile::readFile(args.value()[3]);
 
     if(!peaceTermsFile.has_value() || !warTermsFile.has_value() || !textFile.has_value()) {
+        std::cout << "Failed to read file" << std::endl;
         return 1;
     }
 
