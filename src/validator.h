b95da21d61b11bb9173be564a4f34bf1f2b031bc
Leonard Struck <me@leonardstruck.com>
2023-11-27 18:32:49 +0100

    remove const

--- src/validator.h
+++ src/validator.h
@@ -8,7 +8,7 @@
 #include "readFile.h"
 
 namespace validator {
-  auto validateArgs = [](const int argc, const char* argv[]) -> std::optional<std::vector<std::string>> {
+  auto validateArgs = [](int argc, char* argv[]) -> std::optional<std::vector<std::string>> {
     // es müssen genau drei Dateien übergeben werden (book, war terms, peace terms)
     if (argc != 4) {
         return std::nullopt;
